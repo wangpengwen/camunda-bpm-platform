5265b1217acb3bf8619a8c9605e68e4dc61db23c
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2020-03-13 16:26:49 +0100

    wip: set up license generator; remove unnecessary dependencies

--- distro/license-book-generated/src/main/java/org/camunda/bpm/licensebook/LicenseBookGenerator.java
+++ distro/license-book-generated/src/main/java/org/camunda/bpm/licensebook/LicenseBookGenerator.java
@@ -1,12 +1,24 @@
 package org.camunda.bpm.licensebook;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.StringJoiner;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
 
@@ -21,19 +33,99 @@ public class LicenseBookGenerator {
 	 */
 	
 	public static void main(String[] args) throws IOException {
-		if (args.length != 2) {
-			throw new RuntimeException("Requires 1 argument: The directory with the input files");
+		if (args.length != 3) {
+			throw new RuntimeException("Requires 3 arguments: The directory with the input files, "
+			    + "the root directory for the license info, "
+			    + "the output file name");
 		}
 		
 		String inputDirName = args[0];
-		String licensesDir = args[1];
+		String licensesDirName = args[1];
+		String outFileName = args[2];
 		
 		File inputDir = new File(inputDirName);
+		Path licensesDir = new File(licensesDirName).toPath();
+		File outFile = new File(outFileName);
+
 		Collection<File> inputFiles = FileUtils.listFiles(inputDir, new String[] { "txt" }, false);
 		
-		Map<MavenArtifact, List<CamundaModule>> artifactsUsage = new TreeMap<>();
+		Map<MavenArtifact, List<CamundaModule>> artifactsUsage = parseDependencies(inputFiles);
 
-		for (File camundaModuleDescriptor : inputFiles) {
+		System.out.println(artifactsUsage);
+
+		FileUtils.forceMkdir(outFile.getParentFile());
+		// TODO: close resources
+		OutputStream outFileStream = Files.newOutputStream(outFile.toPath());
+		BufferedOutputStream bufferedOutFileStream = new BufferedOutputStream(outFileStream);
+		OutputStreamWriter streamWriter = new OutputStreamWriter(bufferedOutFileStream);
+
+		
+		List<MavenArtifact> artifactsWithoutLicensingInfos = new ArrayList<MavenArtifact>();
+		
+		for (Map.Entry<MavenArtifact, List<CamundaModule>> dependencyUsage : artifactsUsage.entrySet()) {
+		  MavenArtifact dependency = dependencyUsage.getKey();
+		  streamWriter.write(dependency.toString());
+		  streamWriter.write("\n");
+		  List<CamundaModule> usingModules = dependencyUsage.getValue();
+		  String modulesList = usingModules.stream().map(Object::toString).collect(Collectors.joining(",\n\t"));
+		  streamWriter.write("Used by Camunda modules:\n\t");
+		  streamWriter.write(modulesList);
+		  streamWriter.write("\n");
+		  
+		  String[] groupId = dependency.getGroupId().split("\\.");
+		  // TODO: check file delimiter
+		  String dependencySubPath = String.join("/", groupId) + "/" 
+		      + dependency.getArtifactId() + "/"
+		      + dependency.getVersion();
+		  
+		  Path dependencyDirPath = licensesDir.resolve(dependencySubPath);
+		  
+		  File dependencyDir = dependencyDirPath.toFile();
+	    streamWriter.flush(); // TODO: not great
+		  
+		  if (dependencyDir.exists()) {
+		    Collection<File> contentFiles = FileUtils.listFiles(dependencyDir, null, false);
+		    // TODO: consider ordering them alphabetically
+
+		    if (contentFiles.isEmpty()) {
+		      artifactsWithoutLicensingInfos.add(dependency);
+		    }
+		    
+		    for (File file : contentFiles) {
+		      FileInputStream inputStream = FileUtils.openInputStream(file);
+		      
+		      // TODO: put somwhere central
+		      byte[] buffer = new byte[1024];
+		      int length;
+		      while ((length = inputStream.read(buffer)) > 0) {
+		        bufferedOutFileStream.write(buffer, 0, length);
+		      }
+		    }
+		    streamWriter.write("\n");
+		  }
+		  else {
+		    artifactsWithoutLicensingInfos.add(dependency);
+		  }
+		  
+      streamWriter.write("\n");
+		}
+		
+		streamWriter.write("Artifacts without licensing information:\n");
+		for (MavenArtifact artifact : artifactsWithoutLicensingInfos) {
+		  streamWriter.write(artifact.toString());
+		  streamWriter.write("\n");
+		}
+		
+		streamWriter.flush();
+		bufferedOutFileStream.flush();
+		outFileStream.flush();
+		outFileStream.close();
+	}
+
+  private static Map<MavenArtifact, List<CamundaModule>> parseDependencies(Collection<File> inputFiles) throws IOException {
+    
+    Map<MavenArtifact, List<CamundaModule>> artifactsUsage = new TreeMap<>();
+    for (File camundaModuleDescriptor : inputFiles) {
 			List<String> dependencyCoordinates = FileUtils.readLines(camundaModuleDescriptor);
 			
 			String fileName = camundaModuleDescriptor.getName();
@@ -59,7 +151,7 @@ public class LicenseBookGenerator {
 				usingModules.add(camundaModule);
 			}
 		}
-		
-		System.out.println(artifactsUsage);
-	}
+    
+    return artifactsUsage;
+  }
 }
